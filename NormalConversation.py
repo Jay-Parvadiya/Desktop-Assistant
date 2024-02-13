550f2c9fd142915ec231b821670b66c8ed6037fc
22IT089-Jay_Parvadiya <147327223+Jay-Parvadiya@users.noreply.github.com>
2024-02-13 21:57:29 +0530

    Add web task file and normal conversation file

---
+++ NormalConversation.py
@@ -0,0 +1,48 @@
+from SGPMain import takeCommand,speak,VAName
+import openai
+
+openai.api_key = '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'
+
+# This function interct with chatGPT and generat answer  based on user input
+def chat_with_gpt(query):
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+          {
+            "role": "user",
+            "content": query
+          }
+        ],
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response.choices[0].message.content.strip()
+
+def Conversetion(query):
+    if query in ['bye', 'exit', f'{VAName} exit',f'bye {VAName}'] :
+        answer = chat_with_gpt(query)
+        print(f"{VAName} : ",answer)
+        speak(answer)
+        exit()
+    elif query in ['who are you','who r u','hu r u']:
+        print(f'{VAName} : I am a Virtual Assistant developed by AI Developers\n')
+        speak("I am a Virtual Assistant developed by AI Developers")
+    else:
+        answer = chat_with_gpt(query)
+        print(f"{VAName} :",answer)
+        speak(answer)
+     
+if __name__ == '__main__':
+
+    query = takeCommand()
+    Conversetion(query)
+
+
+# query = takeCommand().lower()
+
+# if 'hello' in query:
+#     print(f'{VAName} : Hello! How can I help you?')
+#     speak("Hello! How can I help you?")
