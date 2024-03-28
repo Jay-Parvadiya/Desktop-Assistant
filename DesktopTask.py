c37f013871f850eb6c32a0b97b606ac7b1539087
22IT085 - JAYDIP <147590189+Pansuriya123@users.noreply.github.com>
2024-03-28 20:14:30 +0530

    update DesktopTask
    changes in split function for word,powerpoint etc.
    whatsappTask

--- DesktopTask.py
+++ DesktopTask.py
@@ -1,4 +1,4 @@
-from SGPMain import speak,VAName
+from SGPMain import speak,VAName,takeCommand
 import os
 import pyautogui
 from PIL import ImageGrab
@@ -46,16 +46,27 @@ def openApp(query):
 
     else:
         app_name = query.split(' ')
-        app_name = app_name[app_name.index('open') + 1]
+        try:
+             
+            app_name = app_name[app_name.index('open') + 1]
+        except Exception as e:
+             print(f"{VAName} : No application name provided")
+             speak(f" something wrong ") 
+             
         print(f'{VAName} : Opening {app_name}')
         speak(f"Opening {app_name}")
-        try:
-            os.system(f'start {app_name}')
-        except Exception as e:
-            print(f'{VAName} : {app_name} not found')
-            speak(f'{app_name} not found')
-            print(e)
-
+        #try:
+            #os.system(f'start {app_name}')
+        #except Exception as e:
+        pyautogui.press('win')
+            
+        if len(app_name)>0 :
+                pyautogui.typewrite(app_name)
+                pyautogui.sleep(0.5)
+                pyautogui.press('enter')
+        else:
+                print(f"{VAName} : No application name provided")
+                speak(f" something wrong ")
 #==========================================================================================================
 def closeApp(query):
     app_name = query.split(' ')
@@ -68,6 +79,43 @@ def closeApp(query):
         print(f'{VAName} : {app_name} not found')
         speak(f'{app_name} not found')
         print(e)
+#==========================================================================================================
+def whatsappTask(query):
+    if f"{VAName} open whatsapp and send massage to" in query:  # This is use for massage anyone on whatsapp only # This only work when whatsapp is off and video call or voice call is off
+        array = query.split()
+        input_name = array[array.index('open') + 1] # This is use for take name of person for massage
+        print(f"{VAName} : Opening {input_name}")
+        speak(f"Opening {input_name}")
+ 
+        pyautogui.keyDown('win')
+        pyautogui.press('q')
+        pyautogui.keyUp('win')
+ 
+        if len(input_name)>0 :
+                pyautogui.typewrite(input_name)
+                pyautogui.press('enter')
+        else:
+            print(f"{VAName} : No application name provided")
+        pyautogui.sleep(5)
+ 
+        input_name = array[array.index('to') + 1]
+        pyautogui.typewrite(f'{input_name}')
+        pyautogui.press('enter')
+        pyautogui.sleep(3)
+       
+        print(f'{VAName} : what you whant to send')
+        speak('what you whant to send')
+        query = takeCommand().lower()
+       
+        if query == 'none':
+                print(f'{VAName} : Massage not given')
+        else:
+                pyautogui.typewrite(query)
+                print(f'{VAName} : sending {query}')
+                speak(f'sending {query}')
+                pyautogui.press('enter') 
+
+
 
 #==========================================================================================================
 def shutdownPC(query):
